9dbf12e30d462da46059b73a379048455df1c37e
statueofdavid <dmpersonalemail@gmail.com>
2021-06-05 02:06:39 -0400

    added a comment in oxford_controller

--- oxford_controller.py
+++ oxford_controller.py
@@ -10,9 +10,10 @@ application_key = config['oxford']['application_keys']
 
 language_code = config['oxford']['language_code']
 
+# responds with the root of the word in the signature
 def get_lemma_response(word):
     end_point = config['oxford']['lemma_end_point']
     url = base_url + "/" + end_point + "/" + language_code + "/" + word
     header = {"application_id":application_id,"application_key":application_key}
     r = requests.get(url, headers)
-    return r    
+    return r
