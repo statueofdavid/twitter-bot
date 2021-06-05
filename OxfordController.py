5980c05822b1130f471198d00e253d744bd838d8
statueofdavid <dmpersonalemail@gmail.com>
2021-06-05 01:58:56 -0400

    dunno really just a noob playing round, tryna get better

--- OxfordController.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-import toml
-
-config = toml.load("config.toml")
-
-base_url = config['oxford']['base_url']
-application_id = config['oxford']['application_id']
-application_key = config['oxford']['application_keys']
---
+++ oxford_controller.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import toml
+
+config = toml.load("config.toml")
+
+base_url = config['oxford']['base_url']
+application_id = config['oxford']['application_id']
+application_key = config['oxford']['application_keys']
+
+language_code = config['oxford']['language_code']
+
+def get_lemma_response(word):
+    end_point = config['oxford']['lemma_end_point']
+    url = base_url + "/" + end_point + "/" + language_code + "/" + word
+    header = {"application_id":application_id,"application_key":application_key}
+    r = requests.get(url, headers)
+    return r    
