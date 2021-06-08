af81f54e5ae1bc3c7d7664f53beb328a8e209b04
statueofdavid <dmpersonalemail@gmail.com>
2021-06-07 23:22:37 -0400

    added try-catch in oxford_controller added plans for site scrapper

--- oxford_controller.py
+++ oxford_controller.py
@@ -11,9 +11,38 @@ application_key = config['oxford']['application_keys']
 language_code = config['oxford']['language_code']
 
 # responds with the root of the word in the signature
-def get_lemma_response(word):
+def get_lemma_payload(word):
     end_point = config['oxford']['lemma_end_point']
     url = base_url + "/" + end_point + "/" + language_code + "/" + word
     header = {"application_id":application_id,"application_key":application_key}
-    r = requests.get(url, headers)
+
+    return url, header
+
+def get_word_data_payload(word):
+    end_point = config['oxford']['word_end_point']
+    url = base_url + "/" + end_point + "/" + language_code + "/" + word
+    header = {"application_id":application_id,"application_key":application_key}
+
+    return url, header
+
+def try_request_handler(payload):
+    url = payload[0]
+    header = payload[1]
+    # some error handling with try-catch
+    try:
+        r = requests.get(url, header)
+    except requests.exceptions.ConnectionError as errconn:
+        return "well something at the network level is jacked"
+    except requests.exceptions.HTTPError as errhttp:
+        return "well http is jacked"
+    except requests.exceptions.Timeout as errto:
+    # Maybe set up for a retry, or continue in a retry loop
+        return "well try again later"
+    except requests.exceptions.TooManyRedirects as errtmr:
+    # Tell the user their URL was bad and try a different one
+        return "well that url is whack"
+    except requests.exceptions.RequestException as errunkn:
+    # catastrophic error. bail.
+        return "shrug"
+        raise SystemExit(e)
     return r
---
+++ web_data_controller.py
@@ -0,0 +1,11 @@
+from lxml import html
+import requests
+
+# navigate to some site ??? respect robot.txt
+# store in list of visited sites
+# parse the html
+# check links in list of already visited sites
+# store new links in a list to visit later
+# ???toss duplicated and visited
+# store all the words... more detail is needed.. how to sort words
+# move on to next in list (hopefully it doesn't run out)
